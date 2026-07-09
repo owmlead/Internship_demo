715136c444e1dc94add4118ab7296ee2d1b5e3ee
owmlead <2318338829@qq.com>
2026-07-09 10:02:20 +0800

    None

--- week_3/demo_3/async_crawler.py
+++ week_3/demo_3/async_crawler.py
@@ -1,16 +1,41 @@
 import asyncio
 import csv
-import re
+import os
+
 import aiohttp
 from lxml import html
 import random
 
+
+# 设置代理信息
+proxyHost = "www.16yun.cn"
+proxyPort = "5445"
+proxyUser = "16QMSOML"
+proxyPass = "280651"
+proxy_url = f"https://{proxyUser}:{proxyPass}@{proxyHost}:{proxyPort}"
+
+
 # 定义URL
-URL = "https://www.themoviedb.org"
+URL = "https://quotes.toscrape.com/page/"
 PHAT = "./test/test.csv"
 
-HEADER = {
-}
+
+
+def header_x():
+    # 随机获取一个headers
+    user_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
+                   'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0'
+                   ]
+
+    headers = {
+        "User-Agent": random.choice(user_agents)
+    }
+    return headers
+
 
 
 class CrawLer:
@@ -23,21 +48,24 @@ class CrawLer:
     async def fetch_url(self, session, url, semaphore):
         async with semaphore:
             try:
-                await asyncio.sleep(random.uniform(0.5, 2))
+                await asyncio.sleep(random.uniform(5, 8))
                 async with session.get(url, timeout=10) as response:
                     html_text = await response.text()
                     return await self.get_data(html_text)
             except Exception as e:
-                return {"error": str(e)}
+                print(f"error: {str(e)}")
+                return []
 
     async def start(self):
-        connector = aiohttp.TCPConnector(limit_per_host=100)
+        resolver = aiohttp.resolver.ThreadedResolver()
+        connector = aiohttp.TCPConnector(limit_per_host=3, resolver=resolver)
         semaphore = asyncio.Semaphore(3)
-        async with aiohttp.ClientSession(headers=HEADER,connector=connector) as session:
+        async with aiohttp.ClientSession(connector=connector,headers=header_x()) as session:
             detail_urls = []
             for page in range(self.page_start, self.page_end + 1):
-                urls = await self.get_movie_page_num(page, session)
-                detail_urls.extend(urls)
+                await asyncio.sleep(random.uniform(3, 5))
+                urls = await self.get_page_num(page, session)
+                detail_urls.extend([urls])
 
             if not detail_urls:
                 print("未获取到任何电影链接")
@@ -46,52 +74,85 @@ class CrawLer:
             tasks = [asyncio.create_task(self.fetch_url(session, url, semaphore)) for url in detail_urls]
             results = await asyncio.gather(*tasks)
 
+            flat_data = []
             valid_results = [r for r in results if r is not None]
-            if valid_results:
-                self.up_data(valid_results)
+            for page_data in valid_results:
+                flat_data.extend(page_data)
+            if flat_data:
+                self.up_data(flat_data)
             else:
                 print("没有成功获取到任何电影数据")
 
     @staticmethod
     async def get_data(html_text):
         document = html.fromstring(html_text)
-        movie_name = document.xpath("//*[@id='original_header']/div[2]/section/div[1]/h2/a/text()")
-        name = re.search(r".+", movie_name).group() if movie_name else ""
-        movie_date = document.xpath("//*[@id='original_header']/div[2]/section/div[1]/div/span[2]/text()")
-        date = re.search(r"\d{4}-\d{2}-\d{2}", movie_date).group() if movie_date else ""
-        movie_type = document.xpath("//*[@id='original_header']/div[2]/section/div[1]/div/span[3]/a/text()")
-        type_list = re.findall(r"\w{2}", movie_type)
-        types = []
-        for t in type_list:
-            types.append(t)
-        types = ','.join(types)
-        movie_time = document.xpath("//*[@id='original_header']/div[2]/section/div[1]/div/span[4]/text()")
-        time = str(
-            int(re.search(r"\d*h", movie_time).group()[:-1] if re.search(r"\d*h",
-                                                                         str(movie_time)) else 0) * 60 + int(
-                re.search(r"\d*m", movie_time).group()[:-1] if re.search(r"\d*m", str(movie_time)) else 0))
-        movie_grade = document.xpath("//*[@id='consensus_pill']/div/div[1]/div/div/@data-percent")
-        grade = re.search(r"\d{1,2}", movie_grade).group() if movie_grade else ""
-        movie_slogan = document.xpath("//*[@id='original_header']/div[2]/section/div[3]/h3[1]/text()")
-        slogan = re.sub(r'^.{2}|.{2}$', "", re.search(r".*", movie_slogan).group()) if movie_slogan else ""
-        movie_summary = document.xpath("//*[@id='original_header']/div[2]/section/div[3]/div/p/text()")
-        summary = re.sub(r'^.{2}|.{2}$', "", re.search(r".*", movie_summary).group()) if movie_summary else ""
-        movie_director = document.xpath("//*[@id='original_header']/div[2]/section/div[3]/ol/li[1]/p[1]/a/text()")
-        director = re.sub(r'^.{2}|.{2}$', "", re.search(r".*", movie_director).group()) if movie_director else ""
-        movie_screenwriter = document.xpath(
-            "//*[@id='original_header']/div[2]/section/div[3]/ol/li[2]/p[1]/a/text()")
-        screenwriter = re.sub(r'^.{2}|.{2}$', "",
-                              re.search(r".*", movie_screenwriter).group()) if movie_screenwriter else ""
-        movie = (
-            {"名字": name, "日期": date, "类型": types, "时长": time, "评分": grade, "评语": slogan,
-             "简介": summary, "导演": director, "制片人": screenwriter})
-        print(movie)
-        return movie
+        # name = document.xpath("//*[@id='wrapper']/h1/span/text()")
+        #
+        # writer = document.xpath("//*[@id='info']/span[1]/span/a/text()")
+        #
+        # translator = document.xpath("//*[@id='info']/span[2]/a/text()")
+        #
+        # publishing_house = document.xpath("//*[@id='info']/span[3]/a/text()")
+        #
+        # production_company = document.xpath("//*[@id='info']/span[4]/a/text()")
+        #
+        # date = document.xpath("//*[@id='info']/span[5]/a/text()")
+        #
+        # isbn = document.xpath("//*[@id='info']/span[6]/a/text()")
+        #
+        # page_count = document.xpath("//*[@id='info']/span[7]/a/text()")
+        #
+        # book_binding = document.xpath("//*[@id='info']/span[8]/a/text()")
+        #
+        # price = document.xpath("//*[@id='info']/span[9]/a/text()")
+        #
+        # book_series = document.xpath("//*[@id='info']/span[10]/a/text()")
+        # book = (
+        #     {
+        #         "名字": name,
+        #         "作者": writer,
+        #         "译者": translator,
+        #         "出版社": publishing_house,
+        #         "出品方": production_company,
+        #         "出版年": date,
+        #         "ISBN": isbn,
+        #         "页数": page_count,
+        #         "装帧": book_binding,
+        #         "定价": price,
+        #         "丛书": book_series
+        #     }
+        # )
+        quote=[]
+        for i in range(1,11):
+            content = document.xpath(f"//div[@class='col-md-8']/div[{i}]/span[1]/text()")[0]
+            if content:
+                # 去除首尾的中文引号、英文引号
+                content = content.strip('“”\'"')
+                # 压缩多个空白为单个空格
+                content = ' '.join(content.split())
+            else:
+                content = ""
+            author = document.xpath(f"//div[@class='col-md-8']/div[{i}]/span[2]/small/text()")[0]
+            author = author.strip() if author else ""
+            tags = document.xpath(f"//div[@class='col-md-8']/div[{i}]/div/a/text()")
+            if tags:
+                # 去除每个标签首尾空格，并用逗号连接
+                tags = ', '.join([tag.strip() for tag in tags])
+            else:
+                tags = ""
+            quote.append({
+                "内容": content,
+                "作者": author,
+                "标签": tags
+            })
+        return quote
 
     # 上传数据到文件
     def up_data(self, movie_data):
         if not movie_data:
             return
+        if not os.path.exists(os.path.dirname(self.save_path)):
+            os.makedirs(os.path.dirname(self.save_path))
         with open(self.save_path, "w", encoding="utf-8", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=movie_data[0].keys())
             writer.writeheader()
@@ -104,21 +165,21 @@ class CrawLer:
             return list(reader)
 
     # 获取电影url数据
-    async def get_movie_page_num(self, page, session):
+    async def get_page_num(self, page, session):
         # 发送请求，获取数据
-        url=f"{URL}/movie?page={page}"
-        await asyncio.sleep(random.uniform(1, 3))
-        async with session.get(url, timeout=60) as response:
-            # 输出数据
-            html_text = await response.text()
-            document = html.fromstring(html_text)
-            # 电影URL
-            movie_url_list = document.xpath(
-                f"//*[@id='page_{page}']//div[@class='relative w-full']/a[@data-media-type='movie']/@href")
-            return  movie_url_list
+        url = f"{URL}{page}/"
+        # await asyncio.sleep(random.uniform(1, 3))
+        # async with session.get(url, timeout=10) as response:
+        #     # 输出数据
+        #     html_text = await response.text()
+        #     document = html.fromstring(html_text)
+        #     # URL
+        #     url_list = document.xpath(
+        #         f"//*[@id='content']/div[2]/div[1]/ul//a[@class='fleft']/@href")
 
+        return url
 
 
 if __name__ == '__main__':
-    x = CrawLer(URL, PHAT, 1, 2)
+    x = CrawLer(URL, PHAT, 1, 10)
     asyncio.run(x.start())
